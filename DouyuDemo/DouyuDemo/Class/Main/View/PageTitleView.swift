31bb99dd600f906ef308dd1bc76dc43371035668
GreatSKL <49362109@qq.com>
2017-08-06 22:11:40 +0800

    添加顶部scrollview

---
+++ DouyuDemo/DouyuDemo/Class/Main/View/PageTitleView.swift
@@ -0,0 +1,21 @@
+//
+//  PageTitleView.swift
+//  DouyuDemo
+//
+//  Created by GreatSKL on 2017/8/6.
+//  Copyright © 2017年 GreatSKL. All rights reserved.
+//
+
+import UIKit
+
+class PageTitleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
