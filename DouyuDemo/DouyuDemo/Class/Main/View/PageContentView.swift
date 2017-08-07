f0373150d9bacfb3828a358369483bbe1f6e45f9
GreatSKL <49362109@qq.com>
2017-08-07 21:23:36 +0800

    添加pagecontentview

---
+++ DouyuDemo/DouyuDemo/Class/Main/View/PageContentView.swift
@@ -0,0 +1,21 @@
+//
+//  PageContentView.swift
+//  DouyuDemo
+//
+//  Created by GreatSKL on 2017/8/7.
+//  Copyright © 2017年 GreatSKL. All rights reserved.
+//
+
+import UIKit
+
+class PageContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DouyuDemo/DouyuDemo/Class/Tools/Extension/UIColor-Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UIColor-Extension.swift
+//  DouyuDemo
+//
+//  Created by GreatSKL on 2017/8/7.
+//  Copyright © 2017年 GreatSKL. All rights reserved.
+//
+
+import Foundation
